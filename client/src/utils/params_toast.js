c421eeca82683a79fa12a5f2688cdb698294768a
Nikita <nikki-xit@mail.ru>
2023-06-09 03:15:35 +0300

    adding notification message

---
+++ client/src/utils/params_toast.js
@@ -0,0 +1,41 @@
+import { ToastContainer } from "react-toastify";
+
+const ToastContainers = () => (
+  <>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  </>
+);
+export default ToastContainers;
+
+export const TOAST_SUCCESS = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+};
+
+export const TOAST_ERROR = {
+  position: "top-right",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+};
