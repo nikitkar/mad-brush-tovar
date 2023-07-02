104326b21c7a8a4dfb5fe9820381683dd61a67f5
nikitkar <88081555+nikitkar@users.noreply.github.com>
2023-07-02 15:08:26 +0300

    Delete combined directory

--- combined/routes/productsRouter.js
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-import { ProductController } from "../controllers/ProductController.js";
-
-const productsRouter = new Router();
-const productsController = new ProductController();
-
-productsRouter.get("/", productsController.getAllProduct);
-
-export { productsRouter };
